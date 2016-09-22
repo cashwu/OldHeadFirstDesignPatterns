93b701544d6e0504a6cb34b32be390bfe786e506
Cash Wu <cash@cashwu.com>
2016-09-22 23:36:08 +0800

    Add CurrentConditionsDisplay

---
+++ Ch02 Observer/CurrentConditionsDisplay.cs
@@ -0,0 +1,31 @@
+﻿using Ch02_Observer.@interface;
+using System;
+
+namespace Ch02_Observer
+{
+    public class CurrentConditionsDisplay : IObserver, IDisplayElement
+    {
+        private readonly ISubject _weatherData;
+        private float _temperature;
+        private float _humidity;
+
+        public CurrentConditionsDisplay(ISubject weatherData)
+        {
+            _weatherData = weatherData;
+            weatherData.RegisterObserver(this);
+        }
+
+        public void Update(float temperature, float humidity, float pressure)
+        {
+            this._temperature = temperature;
+            this._humidity = humidity;
+
+            Display();
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Current conditions : {_temperature} F degress and {_humidity} % humidity ");
+        }
+    }
+}
